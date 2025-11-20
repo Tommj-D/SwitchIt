877cf7199f73b672c700db3bb89282ab8cb59d46
Tommj-D <tom.deangelis1@stud.uniroma3.it>
2025-11-20 13:33:44 +0100

    Finite spine

--- Assets/Scripts/EnemyScripts/Spike_Fall.cs
+++ Assets/Scripts/EnemyScripts/Spike_Fall.cs
@@ -1,75 +1,76 @@
 using UnityEngine;
+using System.Collections;
 
 public class Spike_Fall : MonoBehaviour
 {
     public float fallSpeed = 3f;
     public float shakeDuration = 0.3f;
     public float shakeAmount = 0.1f;
+    public GameObject impactParticles;   
 
-    public GameObject impactParticles;
-
-    private bool isShaking = false;
     private bool isFalling = false;
+    private bool isShaking = false;
+    private bool activated = false; // impedisce ri-attivazioni
 
     private Vector3 originalPosition;
-    private Rigidbody2D rb;
 
     void Start()
     {
         originalPosition = transform.position;
-        rb = GetComponent<Rigidbody2D>();
-
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        rb.gravityScale = 0f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         // Tremolio
         if (isShaking)
         {
-            Vector3 shakePos = originalPosition + (Vector3)Random.insideUnitCircle * shakeAmount;
-            rb.MovePosition(shakePos);
+            Vector2 rand = Random.insideUnitCircle * shakeAmount;
+            transform.position = originalPosition + new Vector3(rand.x, rand.y, 0f);
+            /*Vector3 shakePos = originalPosition + (Vector3)Random.insideUnitCircle * shakeAmount;
+            transform.position = shakePos;*/
         }
 
         // Caduta
         if (isFalling)
         {
-            Vector2 newPos = rb.position + Vector2.down * fallSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(newPos);
+            transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+            //transform.Translate(0, -fallSpeed * Time.deltaTime, 0);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !activated)
         {
+            activated = true;
             StartCoroutine(StartShake());
         }
     }
 
-    private System.Collections.IEnumerator StartShake()
+    private IEnumerator StartShake()
     {
         isShaking = true;
 
         yield return new WaitForSeconds(shakeDuration);
 
-        // Fine tremolio
         isShaking = false;
-        rb.MovePosition(originalPosition);
+        transform.position = originalPosition;
 
-        // Inizia la caduta
         isFalling = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Controlla se ha colpito il terreno e fa partire particelle 
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        // Se tocca un oggetto con layer "Ground"
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")||collision.gameObject.CompareTag("Player"))
         {
+            // Istanzia le particelle
             if (impactParticles != null)
+            {
                 Instantiate(impactParticles, transform.position, Quaternion.identity);
+            }
 
+            // Distruggi la spina
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/PlayerScripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerScripts/PlayerRespawn.cs
@@ -1,19 +1,83 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    public Vector3 respawnPoint;
 
-    public void RespawnNow()
+    private PlayerControls controls; // riferimento alla classe PlayerControls generata
+
+    public Vector3 respawnPoint;
+    public float respawnDelay = 1.5f;
+    public GameObject deathParticle;
+
+    private Animator animator;
+    private bool isDying = false;
+
+    /*private void Awake()
     {
-        transform.position = respawnPoint;
+        // Inizializza i controlli
+        controls = new PlayerControls();
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }*/ //Disabilitato perchè in futuro vorrò fermare il gicatore quando muore
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Death"))
+        if (!isDying && collision.gameObject.CompareTag("Death"))
         {
-            RespawnNow();
+            StartCoroutine(DeathSequence());
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isDying && other.gameObject.CompareTag("Death"))
+        {
+            StartCoroutine(DeathSequence());
+        }
+    }
+
+    private IEnumerator DeathSequence()
+    {
+        isDying = true;
+        /*GetComponent<Rigidbody2D>().linearVelocity = 0; // blocca eventuali movimenti
+        GetComponent<Collider2D>().enabled = false; // evita ulteriori collisioni durante morte
+
+        // Disabilita Controller
+        controls.Disable();*/  //Disabilitato perchè in futuro vorrò fermare il gicatore quando muore
+
+        //Lancia animazione di morte
+        if (animator != null)
+            animator.SetTrigger("Death");
+
+        //Particelle
+        if (deathParticle != null)
+            Instantiate(deathParticle, transform.position, Quaternion.identity);
+
+        //Aspetta l'animazione + tempo extra
+        yield return new WaitForSeconds(respawnDelay);
+
+        // Respawn
+        transform.position = respawnPoint;
+
+        //Animazione Respawn
+        if (animator != null)
+            animator.SetTrigger("Respawn");
+
+        //GetComponent<Collider2D>().enabled = true; //Disabilitato perchè in futuro vorrò fermare il gicatore quando muore
+
+        /*// Riattiva controlli
+        controls.Enable();*///Disabilitato perchè in futuro vorrò fermare il gicatore quando muore
+
+        isDying = false;
     }
 }
